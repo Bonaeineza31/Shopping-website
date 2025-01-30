e0db5c9c348d4cb6bebb8cfb0feb704bbe00fe2b
Ineza Gisubizo Bonae <b.ineza@alustudent.com>
2025-01-30 17:53:56 +0000

    added pictures to the blog

--- assignment/reactwebsite/src/components/Blog.jsx
+++ assignment/reactwebsite/src/components/Blog.jsx
@@ -1,46 +1,52 @@
 import React from 'react';
 import '../styles/blog.css';
+import image6 from '../images/Screenshot 2025-01-30 194318.png'
+import image5 from '../images/Screenshot 2025-01-30 194309.png'
+import image4 from '../images/Screenshot 2025-01-30 194304.png'
+import image3 from '../images/Screenshot 2025-01-30 194259.png'
+import image2 from '../images/Screenshot 2025-01-30 194251.png'
+import image1 from '../images/Screenshot 2025-01-30 194245.png'
 
 const Blog = () => {
   const blogPosts = [
     {
       id: 1,
-      image: '/path-to-wooden-utensils-image.jpg',
+      image: image1,
       date: 'July 18, 2021',
       title: 'Sed magna dui, dignissim id felis vitae, consectetur',
       excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta elit [...]',
     },
     {
       id: 2,
-      image: '/path-to-papers-image.jpg',
+      image: image2 ,
       date: 'July 18, 2021',
       title: 'Fusce suscipit risus tempor, blandit urna at, laoreet ex',
       excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta elit [...]',
     },
     {
       id: 3,
-      image: '/path-to-plant-image.jpg',
+      image: image3 ,
       date: 'July 18, 2021',
       title: 'Pellentesque varius, diam vitae mattis luctus, mi mi cursus',
       excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta elit [...]',
     },
     {
       id: 4,
-      image: '/path-to-clock-image.jpg',
+      image: image4,
       date: 'July 18, 2021',
       title: 'Nam pharetra fringilla justo, et commodo lacus molestie ut',
       excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta elit [...]',
     },
     {
       id: 5,
-      image: '/path-to-coffee-image.jpg',
+      image: image5,
       date: 'July 18, 2021',
       title: 'Fusce vel tellus vulputate, dignissim turpis sit amet, aliquet mi',
       excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta elit [...]',
     },
     {
       id: 6,
-      image: '/path-to-building-image.jpg',
+      image: image6,
       date: 'July 18, 2021',
       title: 'Duis lacinia convallis odio, quis efficitur dolor pulvinar nec',
       excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta elit [...]',
